95c084c531ddc60d6539ddf0a8491f053f04ff91
SobhanDash <sobhan7dash@gmail.com>
2022-06-10 22:32:04 +0530

    fix: mongo uri

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const { MONGO_URI } = require("./config/keys");
-// const mongoURI = MONGO_URI || "mongodb://localhost:27017/kyōyū";
-const mongoURI = "mongodb://localhost:27017/kyōyū";
+const mongoURI = MONGO_URI || "mongodb://localhost:27017/kyōyū";
+// const mongoURI = "mongodb://localhost:27017/kyōyū";
 
 const connectToMongo = async () => {
   try {
